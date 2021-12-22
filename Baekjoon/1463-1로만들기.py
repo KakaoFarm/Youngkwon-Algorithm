cb6d5b65012e3dde2f22c718c86e330c9e198e25
youngkwon02 <youngkwon02@naver.com>
2021-12-23 00:50:45 +0900

    solve: baekjoon 1463-1로 만들기

--- Baekjoon/1463-1로만들기.py
+++ Baekjoon/1463-1로만들기.py
@@ -1,32 +1,14 @@
-import sys
-
 def solution():
-    dp = [None] * (10**6)
-    dp[0] = 0
-    dp[1] = 1
-    dp[2] = 1
-    dp[3] = 2
-    
-    n = int(sys.stdin.readline().rstrip())
-    for i in range(4, n):
-        check_flag = True
-        count = 0
-        temp = i
-        while check_flag:
-            if temp % 3 == 0:
-                temp = temp // 3
-                count += 1
-                if dp[temp] != None:
-                    dp[i] = dp[temp] + count
-                    check_flag = False
-            else:
-              break
-        while check_flag:
-            if temp % 2 == 0:
-                temp = temp // 2
-                count += 1
-                if dp[temp] != None:
-                    dp[i] = dp[temp] + count
-                    check_flag = False
-            else:
-              break
+    dp = [None, 0]
+    n = int(input())
+    for i in range(2, n+1):
+        temp = []
+        if i % 3 == 0:
+            temp.append(dp[i//3])
+        if i % 2 == 0:
+            temp.append(dp[i//2])
+        temp.append(dp[i-1])
+        dp.append(min(temp) + 1)
+    print(dp[n])
+
+solution()
