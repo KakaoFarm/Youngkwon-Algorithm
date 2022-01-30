ed2ae2698ad3546f719f0e0ec0bb8331dc56ee72
youngkwon02 <youngkwon02@naver.com>
2022-01-30 13:55:44 +0900

    solve: baekjoon 1500-최대 곱

---
+++ Baekjoon/1500-최대곱.py
@@ -0,0 +1,24 @@
+# 
+# Baekjoon 1500 - 최대 곱
+# Silver 1
+# 최대 곱
+# 
+
+import sys
+
+(S, K) = map(int, sys.stdin.readline().rstrip().split())
+arr = []
+
+for _ in range(K):
+    arr.append(S // K)
+    
+remain = S - ((S // K) * K)
+
+for i in range(remain):
+    arr[i] += 1
+
+answer = 1
+for num in arr:
+    answer *= num
+    
+print(answer)
