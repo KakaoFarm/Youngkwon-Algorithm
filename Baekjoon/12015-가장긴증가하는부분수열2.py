2a0ae2bafa2fe0d22dc2cb55d9bad05587342df6
youngkwon02 <youngkwon02@naver.com>
2022-09-23 19:43:40 +0900

    solve: 12015-가장 긴 증가하는 부분수열2

---
+++ Baekjoon/12015-가장긴증가하는부분수열2.py
@@ -0,0 +1,21 @@
+#
+# Baekjoon 12015 - 가장 긴 증가하는 부분 수열 2
+# Gold 2
+#
+#
+
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+LIS = [0]
+
+for elem in A:
+    if elem > LIS[-1]:
+        LIS.append(elem)
+    else:
+        LIS[bisect_left(LIS, elem)] = elem
+
+print(len(LIS) - 1)
