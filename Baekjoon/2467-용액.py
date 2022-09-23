157392e2759ce79b1caf82525eaa57bb5fb60095
youngkwon02 <youngkwon02@naver.com>
2022-09-23 17:52:36 +0900

    solve: 2467-용액

---
+++ Baekjoon/2467-용액.py
@@ -0,0 +1,32 @@
+#
+# Baekjoon 2467 - 용액
+# Gold 5
+# 이분 탐색
+#
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = list(map(int, input().split()))
+lst.sort()
+
+start = 0
+end = N - 1
+
+nearest = float('inf')
+answer = [-1, -1]
+while True:
+    if end <= start:
+        break
+    value = lst[start] + lst[end]
+    if abs(value) < nearest:
+        nearest = abs(value)
+        answer = [lst[start], lst[end]]
+    if value > 0:
+        end -= 1
+    elif value < 0:
+        start += 1
+    else:
+        break
+print(answer[0], answer[1])
